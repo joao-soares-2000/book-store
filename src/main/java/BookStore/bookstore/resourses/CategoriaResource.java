98fa2d8d7078c8aca2a2fea64cf2ef6086d36fe2
joao-soares-2000 <joao.159.soares@gmail.com>
2022-08-08 17:21:56 -0300

    findAll DTO

--- src/main/java/BookStore/bookstore/resourses/CategoriaResource.java
+++ src/main/java/BookStore/bookstore/resourses/CategoriaResource.java
@@ -1,6 +1,7 @@
 package BookStore.bookstore.resourses;
 
 import BookStore.bookstore.Domain.Categoria;
+import BookStore.bookstore.dtos.CategoriaDTO;
 import BookStore.bookstore.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -20,5 +24,11 @@ public class CategoriaResource {
     public ResponseEntity<Categoria> findById (@PathVariable Integer id){
         Categoria obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+    @GetMapping
+    public ResponseEntity<List<CategoriaDTO>> findAll() {
+        List<Categoria> list = service.findAll();
+        List<CategoriaDTO> listDTO = list.stream().map(CategoriaDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
