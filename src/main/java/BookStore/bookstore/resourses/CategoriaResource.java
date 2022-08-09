517825c3ee52ac95ce0822c2fe8c8515d1ef5eef
joao-soares-2000 <joao.159.soares@gmail.com>
2022-08-09 10:10:40 -0300

    metodo Post

--- src/main/java/BookStore/bookstore/resourses/CategoriaResource.java
+++ src/main/java/BookStore/bookstore/resourses/CategoriaResource.java
@@ -5,11 +5,10 @@ import BookStore.bookstore.dtos.CategoriaDTO;
 import BookStore.bookstore.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,5 +29,12 @@ public class CategoriaResource {
         List<Categoria> list = service.findAll();
         List<CategoriaDTO> listDTO = list.stream().map(CategoriaDTO::new).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<Categoria> create(@RequestBody Categoria obj) {
+        obj = service.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
