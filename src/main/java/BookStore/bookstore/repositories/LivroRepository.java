cd913df50b822eae8b0be3a66d4fcb464c9d2ac3
joao-soares-2000 <joao.159.soares@gmail.com>
2022-08-23 11:01:02 -0300

    metodos put e patch mapping, para dar update total e parcial de registro

--- src/main/java/BookStore/bookstore/repositories/LivroRepository.java
+++ src/main/java/BookStore/bookstore/repositories/LivroRepository.java
@@ -1,7 +1,6 @@
 package BookStore.bookstore.repositories;
 
 import BookStore.bookstore.Domain.Livro;
-import BookStore.bookstore.dtos.LivroDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,6 +12,6 @@ import java.util.List;
 public interface LivroRepository extends JpaRepository<Livro, Integer> {
 
     @Query("SELECT obj FROM Livro obj WHERE obj.categoria.id = :id_cat ORDER BY titulo")
-    List<LivroDTO> findAllByCategoria(@Param (value = "id_cat") Integer id_cat);
+    List<Livro> findAllByCategoria(@Param (value = "id_cat") Integer id_cat);
 
 }
